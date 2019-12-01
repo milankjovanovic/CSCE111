78102f48a2fa875b1e9f3954fb895ae32fd352e9
milankjovanovic <56699365+milankjovanovic@users.noreply.github.com>
2019-12-01 01:43:02 -0600

    Add files via upload

--- JingleBellsMain.java
+++ JingleBellsMain.java
@@ -2,18 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JingleBellsMain extends JFrame {
-  private JPanel pane = new JPanel(new BorderLayout());
+  private JPanel pane = new JPanel();
 
   private JButton websiteButton = new JButton("Website");
   private JButton videoButton = new JButton("Video");
+  private JButton animationButton = new JButton("Animation");
   private JButton triviaGameButton = new JButton("Trivia Game");
 
   public JingleBellsMain() {
     super("Jingle Bells");
 
-    pane.add(websiteButton, BorderLayout.CENTER);
-    pane.add(videoButton, BorderLayout.WEST);
-    pane.add(triviaGameButton, BorderLayout.EAST);
+    pane.add(videoButton);
+    pane.add(animationButton);
+    pane.add(websiteButton);
+    pane.add(triviaGameButton);
 
     add(pane, BorderLayout.CENTER);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,13 +35,20 @@ public class JingleBellsMain extends JFrame {
 
 
   public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (UnsupportedLookAndFeelException e) {
+    } catch (ClassNotFoundException e) {
+    } catch (InstantiationException e) {
+    } catch (IllegalAccessException e) {
+    }
     JingleBellsMain main = new JingleBellsMain();
     main.run();
     main.setVisible(true);
   }
 
   private void run() {
-    WebsiteClass websiteClass = new WebsiteClass("https://www.google.com/");
+    WebsiteClass websiteClass = new WebsiteClass("http://people.tamu.edu/~milankjovanovic/JingleBells/JingleBellsSong.html");
     VideoClass videoClass = new VideoClass("https://www.youtube.com/watch?v=fkhKg-WLohk");
     TriviaGame triviaGameClass = new TriviaGame();
 
