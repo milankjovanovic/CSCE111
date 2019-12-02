1ff82afa41722d564a59c3b9a6f7a8d172a6b352
brennamc01 <56699791+brennamc01@users.noreply.github.com>
2019-12-02 14:20:33 -0600

    Add files via upload

--- JingleBellsMain.java
+++ JingleBellsMain.java
@@ -3,20 +3,37 @@ import java.awt.*;
 
 public class JingleBellsMain extends JFrame {
   private JPanel pane = new JPanel();
+  private JPanel textPane = new JPanel();
 
   private JButton websiteButton = new JButton("Website");
   private JButton videoButton = new JButton("Video");
   private JButton animationButton = new JButton("Animation");
   private JButton triviaGameButton = new JButton("Trivia Game");
+  private JLabel jlabel = new JLabel("Jingle Bells");
 
   public JingleBellsMain() {
     super("Jingle Bells");
 
+    jlabel.setFont(new Font("Verdana",1,20));
+    jlabel.setForeground(Color.white);
+    textPane.add(jlabel);
     pane.add(videoButton);
     pane.add(animationButton);
     pane.add(websiteButton);
     pane.add(triviaGameButton);
 
+
+    // setBackground(new Color(43, 119, 197));
+    getContentPane().setBackground(new Color(43, 119, 197));
+    pane.setOpaque(false);
+    textPane.setOpaque(false);
+    pane.setLayout( new FlowLayout(FlowLayout.CENTER, 3, 175));
+    websiteButton.setPreferredSize( new Dimension(100, 30));
+    videoButton.setPreferredSize( new Dimension(100, 30));
+    animationButton.setPreferredSize( new Dimension(100, 30));
+    triviaGameButton.setPreferredSize( new Dimension(100, 30));
+
+    add(textPane, BorderLayout.NORTH);
     add(pane, BorderLayout.CENTER);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setPreferredSize(new Dimension(500, 500));
